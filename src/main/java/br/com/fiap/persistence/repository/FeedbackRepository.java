2f2c3760bdfd0d7b335a11da9e42686685602da8
Felipe Nucci <flpnucci@gmail.com>
2026-01-02 17:35:06 -0300

    feature: correção nas dependencias e nas propriedades do BD

--- src/main/java/br/com/fiap/persistence/repository/FeedbackRepository.java
+++ src/main/java/br/com/fiap/persistence/repository/FeedbackRepository.java
@@ -4,9 +4,11 @@ import br.com.fiap.model.dto.FeedbackReportRequest;
 import br.com.fiap.persistence.entity.Feedback;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import io.quarkus.panache.common.Parameters;
+import jakarta.enterprise.context.ApplicationScoped;
 
 import java.util.List;
 
+@ApplicationScoped
 public class FeedbackRepository implements PanacheRepository<Feedback> {
 
     public List<Feedback> getReportsFromPeriod(FeedbackReportRequest request) {
