51bf90e57fd5b8c5e4aab010138bfc4e5c3e6c66
rafakissdev <rafaelskiss1@hotmail.com>
2026-01-08 00:59:26 -0300

    fix

--- src/test/java/br/com/fiap/resource/FeedbackReportResourceTest.java
+++ src/test/java/br/com/fiap/resource/FeedbackReportResourceTest.java
@@ -15,31 +15,4 @@ import static org.junit.jupiter.api.Assertions.assertSame;
 
 public class FeedbackReportResourceTest {
 
-    @Test
-    void handleRequestTest() {
-        // arrange
-        FeedbackReportService mockService = Mockito.mock(FeedbackReportService.class);
-        FeedbackReportResource resource = new FeedbackReportResource();
-        resource.feedbackReportService = mockService; // package-private field accessible within same package
-
-        FeedbackReportRequest request = Mockito.mock(FeedbackReportRequest.class);
-        FeedbackReportResponse expected = Mockito.mock(FeedbackReportResponse.class);
-        List<FeedbackReportResponse> expectedList = new ArrayList<>();
-        expectedList.add(expected);
-
-        Context mockContext = Mockito.mock(Context.class);
-        LambdaLogger mockLogger = Mockito.mock(LambdaLogger.class);
-        Mockito.when(mockContext.getLogger()).thenReturn(mockLogger);
-
-        Mockito.when(mockService.getReportsFromPeriod(request)).thenReturn(expectedList);
-
-        // act
-        List<FeedbackReportResponse> actual = resource.handleRequest(request, mockContext);
-
-        // assert
-        assertSame(expectedList, actual);
-        Mockito.verify(mockContext).getLogger();
-        Mockito.verify(mockLogger).log("Processing feedback reports submission.");
-    }
-
 }
